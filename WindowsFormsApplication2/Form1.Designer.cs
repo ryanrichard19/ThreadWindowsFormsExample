1343470902c4c621397047d2bd9bd37f8774b002
ryanrichard19 <mrrarichard@gmail.com>
2017-02-23 20:12:18 +0200

    Thread example with window forms

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(266, 51);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.Text = "label1";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(266, 79);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(35, 13);
+            this.lbl2.TabIndex = 1;
+            this.lbl2.Text = "label1";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(266, 102);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(35, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "label1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(225, 157);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 322);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread Tread1 = new Thread(UpdateLabel);
+            Tread1.Start(lbl1);
+            Thread Tread2 = new Thread(UpdateLabel);
+            Tread2.Start(lbl2);
+            Thread Tread3 = new Thread(UpdateLabel);
+            Tread3.Start(lbl3);
+
+        }
+
+         void UpdateLabel(object n)
+        {
+            Label statusLabel = n as Label;
+            for (int i = 1; i <= 1000; i++)
+            {
+                int count1 = 0;
+                int count2 = 0;
+                for (int k = 2; k < i - 1; k++)
+                {
+                    int rem = i % k;
+                    if (rem == 0)
+                    {
+                        count1++;
+                    }
+                    else
+                    {
+                        count2++;
+                    }
+                }
+                if (count1 == 0)
+                {
+                    BeginInvoke((Action)(() => statusLabel.Text = i.ToString()));                
+                    Thread.Sleep(500);
+                }
+
+            }
+
+    
+        }
+    }
+}
